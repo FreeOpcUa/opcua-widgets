8670332eaf8ba6b4c011816a400469c0b0d2426c
olivier R-D <olivier@loke>
2016-08-17 22:35:48 +0200

    add setup and release files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(name="opcua-widgets",
+      version="0.1.0",
+      description="OPC-UA Widgets",
+      author="Olivier R-D et al.",
+      url='https://github.com/FreeOpcUa/opcua-widgets',
+      packages=["uawidgets"],
+      license="GNU General Public License",
+      install_requires=["freeopcua"],
+      )
