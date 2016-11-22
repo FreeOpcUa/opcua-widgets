266584c4c032943d086d20fcf28c754fad5c6f5d
olivier R-D <olivier@loke>
2016-11-22 14:44:21 +0100

    make data type and value rank tests work

--- tests.py
+++ tests.py
@@ -24,19 +24,37 @@ class TestAttrsWidget(unittest.TestCase):
     def tearDown(self):
         self.server.stop()
 
+    def find_item(self, text):
+        idxlist = self.widget.model.match(self.widget.model.index(0, 0), Qt.DisplayRole, text,  1, Qt.MatchExactly | Qt.MatchRecursive)
+        idx = idxlist[0]
+        return idx
+
     def modify_item(self, text, val, match_to_use=0):
         """
         modify the current item and set its displayed value to 'val'
         """
-        idxlist = self.widget.model.match(self.widget.model.index(0, 0), Qt.DisplayRole, text, match_to_use + 1, Qt.MatchStartsWith | Qt.MatchRecursive)
+        idxlist = self.widget.model.match(self.widget.model.index(0, 0), Qt.DisplayRole, text, match_to_use + 1, Qt.MatchExactly | Qt.MatchRecursive)
         if not idxlist:
             raise RuntimeError("Item with text '{}' not found".format(text))
         idx = idxlist[match_to_use]
         self.widget.view.setCurrentIndex(idx)
-        idx = idx.sibling(0, 1)
+        idx = idx.sibling(idx.row(), 1)
         self.widget.view.edit(idx)
         editor = self.widget.view.focusWidget()
-        editor.setText(val)
+        if not editor:
+            print(app.focusWidget())
+            print("NO EDITOR WIDGET")
+            #QTest.keyClick(self.widget.view, Qt.Key_Return)
+            from IPython import embed 
+            embed()
+            raise RuntimeError("Could not get editor widget!, it does not have the focus")
+
+        if hasattr(editor, "current_node"):
+            editor.current_node = val
+        elif hasattr(editor, "setCurrentText"):
+            editor.setCurrentText(val)
+        else:
+            editor.setText(val)
         self.widget.view.commitData(editor)
         self.widget.view.closeEditor(editor, QAbstractItemDelegate.NoHint)
         self.widget.view.reset()
@@ -65,14 +83,23 @@ class TestAttrsWidget(unittest.TestCase):
         self.modify_value("titi")
         self.assertEqual(myvar.get_value(), b"titi")
 
-    def ZZZ_test_change_data_type(self):  # need to find a way to modify combo box with QTest
+    def test_change_data_type(self):
         objects = self.server.nodes.objects
         myvar = objects.add_variable(1, "myvar1", 9.99, ua.VariantType.Double)
         self.widget.show_attrs(myvar)
-        self.modify_item("DataType", "Float")
-        print("DT", myvar.get_data_type())
-        self.assertEqual(myvar.get_(), b"titi")
+        dtype = myvar.get_data_type()
+        self.assertEqual(dtype, ua.NodeId(ua.ObjectIds.Double))
+        new_dtype = ua.NodeId(ua.ObjectIds.String)
+        self.modify_item("DataType", self.server.get_node(new_dtype))
+        self.assertEqual(myvar.get_data_type(), new_dtype)
 
+    def test_change_value_rank(self):  # need to find a way to modify combo box with QTest
+        objects = self.server.nodes.objects
+        myvar = objects.add_variable(1, "myvar1", 9.99, ua.VariantType.Double)
+        self.widget.show_attrs(myvar)
+        rank = myvar.get_value_rank()
+        self.modify_item("ValueRank", "ThreeDimensions")
+        self.assertEqual(myvar.get_value_rank(), ua.ValueRank.ThreeDimensions)
 
 
 
