23b527fde05e172ad680400e02258f2abec8b6eb
olivier R-D <olivier@loke>
2016-12-12 17:18:21 +0100

    add logHandler class to share between client and modeler

---
+++ uawidgets/logger.py
@@ -0,0 +1,18 @@
+import logging
+
+
+class QtHandler(logging.Handler):
+
+    def __init__(self, widget):
+        logging.Handler.__init__(self)
+        self.setFormatter(logging.Formatter("%(levelname)s: %(message)s"))
+        self.setFormatter(logging.Formatter("%(name)s - %(levelname)s - %(message)s')"))
+        self.widget = widget
+
+    def emit(self, record):
+        msg = self.format(record)
+        self.widget.append(msg)
+
+
+
+
