0bbfca198029a423fb6dd45fde0f60d7c57a5c00
oroulet <olivier@r-dt.net>
2022-02-13 11:12:39 +0100

    new release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name="opcua-widgets",
-      version="0.6.0",
+      version="0.6.1",
       description="OPC-UA Widgets",
       author="Olivier R-D et al.",
       url='https://github.com/FreeOpcUa/opcua-widgets',
